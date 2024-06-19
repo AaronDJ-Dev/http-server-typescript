7a9b3fec96369a1c81a9a2c849eeac28282079e8
Aaron Jones <aaron.d.jones19@gmail.com>
2024-06-19 17:36:28 -0500

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,47 +1,109 @@
-import * as net from "net";
-import fs from "node:fs";
+import * as net from 'net';
+import * as fs from 'fs';
 
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
-    let splitedData = data.toString().split("\r\n");
-    const path = splitedData[0].split(" ")[1];
-    const headers = splitedData.slice(1, -2);
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (path.indexOf("/echo/") === 0) {
-      const query = path.slice(6);
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${query}`
-      );
-    } else if (path.indexOf("/user-agent") === 0) {
-      const agent = headers
-        .find((h) => h.indexOf("User-Agent: ") === 0)!
-        .slice(12);
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${agent.length}\r\n\r\n${agent}`
-      );
-    } else if (path.startsWith("/files/")) {
-      const [_, __, fileName] = path.split("/");
-      const args = process.argv.slice(2);
-      const [___, absPath] = args;
-      const filePath = absPath + "/" + fileName;
-      try {
-        const content = fs.readFileSync(filePath);
-        socket.write(
-          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`
-        );
-      } catch (error) {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-      }
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-    }
-    socket.end();
-  });
+    socket.on("data", (data) => {
+        const httpResponseBuilder = new HttpResponseBuilder();
+        const [requestLine, ...headers] = data.toString().split("\r\n");
+        const [body] = headers.splice(headers.length - 1);
+        const [method, path] = requestLine.split(" ");
+        const [root, pathRoute, content, ...restParameters] = path.split('/');
+        if (pathRoute === '') {
+            httpResponseBuilder.setStatusLine('HTTP/1.1 200 OK');
+        } else if (pathRoute === 'echo' && content) {
+            httpResponseBuilder.setStatusLine('HTTP/1.1 200 OK');
+            if (content) {
+                const contentLength = content.length;
+                httpResponseBuilder.setHeaders({
+                    'Content-Type': 'text/plain',
+                    'Content-Length': contentLength
+                });
+                httpResponseBuilder.setResponseBody(Buffer.from(content));
+            }
+        } else if (pathRoute === 'user-agent') {
+            httpResponseBuilder.setStatusLine('HTTP/1.1 200 OK');
+            const userAgentHeader = headers.filter(header => {
+                const headerName = header.split(':')[0].toLocaleLowerCase();
+                return headerName === 'user-agent';
+            })[0];
+            const userAgent = userAgentHeader.split(':')[1].trim();
+            const userAgentLength = userAgent.length;
+            httpResponseBuilder.setHeaders({
+                'Content-Type': 'text/plain',
+                'Content-Length': userAgentLength
+            });
+            httpResponseBuilder.setResponseBody(Buffer.from(userAgent));
+        } else if (method === 'GET' && pathRoute === 'files' && content && process.argv[2] === '--directory' && process.argv[3]) {
+            const directoryPath = process.argv[3];
+            const filePath = `${directoryPath}${content}`;
+            try {
+                if (fs.existsSync(filePath)) {
+                    const contents = fs.readFileSync(filePath);
+                    httpResponseBuilder.setStatusLine('HTTP/1.1 200 OK');
+                    httpResponseBuilder.setHeaders({
+                        'Content-Type': 'application/octet-stream',
+                        'Content-Length': contents.length
+                    });
+                    httpResponseBuilder.setResponseBody(contents);
+                } else {
+                    httpResponseBuilder.setStatusLine('HTTP/1.1 404 Not Found');
+                }
+            } catch (err) {
+                httpResponseBuilder.setStatusLine('HTTP/1.1 404 Not Found');
+            }
+        } else if (method === 'POST' && pathRoute === 'files' && content && process.argv[2] === '--directory' && process.argv[3]) {
+            const directoryPath = process.argv[3];
+            const filePath = `${directoryPath}/${content}`;
+            try {
+                fs.writeFileSync(filePath, body);
+                httpResponseBuilder.setStatusLine('HTTP/1.1 201 Created');
+            } catch (err) {
+                httpResponseBuilder.setStatusLine('HTTP/1.1 404 Not Found');
+            }
+        } else {
+            httpResponseBuilder.setStatusLine('HTTP/1.1 404 Not Found');
+        }
+        const httpResponse = httpResponseBuilder.buildHttpResponse();
+        console.log(httpResponse);
+        socket.write(httpResponse);
+        socket.end();
+    });
 });
+
+type Headers = {
+    [key: string]: string | number;
+}
+
+class HttpResponseBuilder {
+    statusLine: string;
+    headers: string;
+    responseBody: Buffer;
+    httpResponse: string | undefined;
+    constructor() {
+        this.statusLine = '';
+        this.responseBody = Buffer.from('');
+        this.headers = '';
+    }
+    buildHttpResponse() {
+        this.httpResponse = `${this.statusLine}\r\n${this.headers}\r\n${this.responseBody}`;
+        return this.httpResponse;
+    }
+    setStatusLine(statusLine: string) {
+        this.statusLine = statusLine;
+    }
+    setHeaders(headers: Headers) {
+        this.headers = '';
+        for (const header in headers) {
+            this.headers += `${header}: ${headers[header]}\r\n`;
+        }
+    }
+    setResponseBody(responseBody: Buffer) {
+        this.responseBody = responseBody;
+    }
+}
 
 console.log("Logs from your program will appear here!");
 
-server.listen(4221, "localhost", () => {
-  console.log("Server is running on port 4221");
+server.listen(4221, 'localhost', () => {
+    console.log('Server is running on port 4221');
 });
