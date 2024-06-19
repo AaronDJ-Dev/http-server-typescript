a921eda2a3f5750a0eeec94075f1e2f1264a950b
Aaron Jones <aaron.d.jones19@gmail.com>
2024-06-19 17:32:12 -0500

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,47 +1,47 @@
-import * as net from 'node:net';
+import * as net from "net";
+import fs from "node:fs";
 
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        const request = data.toString();
-        console.log(request);
-        const path = request.split(' ')[1];
-        console.log(path.split('/')[1])
-        const params = path.split('/')[1];
-        let response: string;
-        function changeResponse(response: string): void {
-            socket.write(response);
-            socket.end();
-        }
-        switch (params) {
-            case '': {
-                response = 'HTTP/1.1 200 OK\r\n\r\n'
-                changeResponse(response)
-                break;
-            }
-            case 'echo': {
-                const message = path.split('/')[2]
-                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
-                changeResponse(response)
-                break;
-            }
-            case 'user-agent': {
-                const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0]
-                response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-                changeResponse(response)
-                break;
-            }
-            default: {
-                response = 'HTTP/1.1 404 Not Found\r\n\r\n'
-                changeResponse(response)
-                break;
-            }
-        }
-        socket.end()
-    })
+  socket.on("data", (data) => {
+    let splitedData = data.toString().split("\r\n");
+    const path = splitedData[0].split(" ")[1];
+    const headers = splitedData.slice(1, -2);
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (path.indexOf("/echo/") === 0) {
+      const query = path.slice(6);
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${query}`
+      );
+    } else if (path.indexOf("/user-agent") === 0) {
+      const agent = headers
+        .find((h) => h.indexOf("User-Agent: ") === 0)!
+        .slice(12);
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${agent.length}\r\n\r\n${agent}`
+      );
+    } else if (path.startsWith("/files/")) {
+      const [_, __, fileName] = path.split("/");
+      const args = process.argv.slice(2);
+      const [___, absPath] = args;
+      const filePath = absPath + "/" + fileName;
+      try {
+        const content = fs.readFileSync(filePath);
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+        );
+      } catch (error) {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+    socket.end();
+  });
 });
 
 console.log("Logs from your program will appear here!");
 
-server.listen(4221, 'localhost', () => {
-    console.log('Server is running on port 4221');
+server.listen(4221, "localhost", () => {
+  console.log("Server is running on port 4221");
 });
